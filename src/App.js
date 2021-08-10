743bd22baf84b61ecf95d2ee247e531fb90abbca
Alan Carpenter <alan.carpenter@trov.com>
2021-08-10 09:42:13 -0700

    pull all songs, check for expired token

--- src/App.js
+++ src/App.js
@@ -3,26 +3,50 @@ import FetchUserData from "./FetchUserToken";
 import AuthButton from "./Components/AuthButton";
 
 function App() {
+  const spotUrl = "https://api.spotify.com/v1/me/tracks";
   const user = FetchUserData("token");
   const [userInfo, setUserInfo] = useState();
 
-  function fetchData(e, offsetAmount = 0) {
-    fetch(
-      `https://api.spotify.com/v1/me/tracks?offset=${offsetAmount}&limit=50`,
-      {
-        headers: {
-          Authorization: `${user.token_type} ${user.token}`,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => setUserInfo(data));
-
-    checkFetchPagination();
+  function updateInfo(data) {
+    setUserInfo(data);
+    return;
   }
 
-  function checkFetchPagination() {
-    // if(userInfo.)
+  async function spotCall(offset = 0) {
+    let response = await fetch(`${spotUrl}?limit=50&offset=${offset}`, {
+      headers: {
+        Authorization: `${user.token_type} ${user.token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error, status: ${response.status}`);
+    }
+
+    let userData = await response.json();
+
+    return userData;
+  }
+  async function fetchData(e) {
+    const userData = await spotCall();
+    let userSongs = [...userData.items];
+
+    if (userData.total % 50 >= 0) {
+      const numberOfCalls = Math.ceil(userData.total / 50);
+      for (let i = 1; i <= numberOfCalls; i++) {
+        const offset = i * 50;
+        if (offset < userData.total) {
+          const nextPage = await spotCall(offset);
+          const pageSongs = nextPage.items;
+          for (let j = 0; j < pageSongs.length; j++) {
+            userSongs.push(pageSongs[j]);
+          }
+        } else {
+          break;
+        }
+      }
+      updateInfo(userSongs);
+    }
   }
 
   return (
@@ -31,7 +55,7 @@ function App() {
       <button onClick={fetchData}>Click to get info</button>
       {typeof userInfo !== "undefined" && (
         <ul>
-          {userInfo.items.map((song) => (
+          {userInfo.map((song) => (
             <li key={song.track.id}>{song.track.name}</li>
           ))}
         </ul>
--- src/FetchUserToken.js
+++ src/FetchUserToken.js
@@ -2,30 +2,40 @@ function setStorage(key, val) {
   window.localStorage.setItem(key, JSON.stringify(val));
 }
 
+async function getSpotifyCreds(key) {
+  // Gather URL pieces
+  const params = new URLSearchParams(window.location.href.split("#")[1]),
+    token = params.get("access_token"),
+    token_type = params.get("token_type"),
+    ttl = params.get("expires_in"),
+    time = new Date();
+
+  // Set token and timestamp
+  const item = {
+    token: token,
+    token_type: token_type,
+    expiry: time.getTime() + parseInt(ttl),
+  };
+
+  // Add to storagge
+  setStorage(key, item);
+
+  return;
+}
+
 export default function FetchUserData(key) {
   if (localStorage.getItem(key)) {
-    // TODO: Check timestamp for validity
     // Get parsed item from storage, return it as string
     const userInfo = JSON.parse(localStorage.getItem("token"));
-    return userInfo;
+    const currentDate = new Date();
+    if (userInfo.expiry > currentDate.getTime()) {
+      return userInfo;
+    } else {
+      getSpotifyCreds(key);
+      return localStorage.getItem("key");
+    }
   } else if (window.location.hash.length > 0) {
-    // Gather URL pieces
-    const params = new URLSearchParams(window.location.href.split("#")[1]),
-      token = params.get("access_token"),
-      token_type = params.get("token_type"),
-      ttl = params.get("expires_in"),
-      time = new Date();
-
-    // Set token and timestamp
-    const item = {
-      token: token,
-      token_type: token_type,
-      expiry: time.getTime() + parseInt(ttl),
-    };
-
-    // Add to storagge
-    setStorage(key, item);
-
+    const token = getSpotifyCreds(key);
     return token;
   }
 }
